a2090301cb990029551710113037f3a5e9dc97f3
hbk671104 <hbk671104@gmail.com>
2018-05-29 23:04:31 +0800

    add invest number

---
+++ app/container/main/dashboard/partials/group/style.js
@@ -0,0 +1,26 @@
+export default {
+	container: {
+		paddingHorizontal: 12
+	},
+	title: {
+		color: '#333333',
+		fontSize: 16,
+		fontWeight: 'bold',
+		marginBottom: 16,
+		marginLeft: 10
+	},
+	card: {
+		margin: 0,
+		padding: 0,
+		// height: 96,
+		// marginHorizontal: 27.5,
+		borderRadius: 7,
+		borderWidth: 0,
+		shadowColor: 'rgba(44, 64, 83, 0.1)',
+		shadowRadius: 4,
+		shadowOffset: {
+			width: 0,
+			height: 2
+		}
+	}
+}
