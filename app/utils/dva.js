493c79506a01e758ecf8c2718fbc2bddb8c6969a
hbk671104 <hbk671104@gmail.com>
2018-05-30 22:36:53 +0800

    loading

--- app/utils/dva.js
+++ app/utils/dva.js
@@ -6,22 +6,22 @@ import createLoading from 'dva-loading'
 export { connect }
 
 export default function(options) {
-  const app = create(options)
-  // HMR workaround
-  if (!global.registered) options.models.forEach(model => app.model(model))
-  global.registered = true
+	const app = create(options)
+	// HMR workaround
+	if (!global.registered) options.models.forEach(model => app.model(model))
+	global.registered = true
 
-  app.use(createLoading({
-    only: [
-      'login/login'
-    ]
-  }))
-  app.start()
-  // eslint-disable-next-line no-underscore-dangle
-  const store = app._store
+	app.use(
+		createLoading({
+			only: ['login/login', 'dashboard/fetch']
+		})
+	)
+	app.start()
+	// eslint-disable-next-line no-underscore-dangle
+	const store = app._store
 
-  app.start = container => () => <Provider store={store}>{container}</Provider>
-  app.getStore = () => store
+	app.start = container => () => <Provider store={store}>{container}</Provider>
+	app.getStore = () => store
 
-  return app
+	return app
 }
