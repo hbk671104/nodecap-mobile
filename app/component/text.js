35588a7d82e1343e1e0d9e97b496af433ef53cac
hbk671104 <hbk671104@gmail.com>
2018-05-31 18:19:38 +0800

    add disablePrefix prop

--- app/component/text.js
+++ app/component/text.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 import { Text } from 'react-native'
 import Accounting from 'accounting'
 
-const numConvert = number => {
+const numConvert = (number, disablePrefix) => {
 	const num = Accounting.formatNumber(number, 0)
+	if (disablePrefix) return num
 	if (number > 0) {
 		return `+${num}`
 	} else {
@@ -16,17 +17,11 @@ const text = props => {
 	const { children, disablePrefix } = props
 	if (typeof children === 'string') {
 		if (!isNaN(children)) {
-			return (
-				<Text {...props}>
-					{disablePrefix ? children : numConvert(children)}
-				</Text>
-			)
+			return <Text {...props}>{numConvert(children, disablePrefix)}</Text>
 		}
 	}
 	if (typeof children === 'number') {
-		return (
-			<Text {...props}>{disablePrefix ? children : numConvert(children)}</Text>
-		)
+		return <Text {...props}>{numConvert(children, disablePrefix)}</Text>
 	}
 	return <Text {...props}>{children}</Text>
 }
