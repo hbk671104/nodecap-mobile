b44982006183912feaf2d6e4c9f81f40f1428869
hbk671104 <hbk671104@gmail.com>
2018-06-04 14:04:08 +0800

    add list component

---
+++ app/component/uikit/color.js
@@ -0,0 +1,17 @@
+export const mainColor = '#0986DB';
+export const backgroundColor = '#F8F8F8';
+export const navBarColor = '#00ABE1';
+export const titleColor = '#333333';
+export const textColor = '#666666';
+export const subTitleColor = '#999999';
+export const placeholderTextColor = '#BBBBBB';
+export const dividerColor = '#EEEEEE';
+export const disabledColor = placeholderTextColor;
+export const borderColor = '#F4F4F5';
+
+// Status Color
+export const interestedColor = '#FFAB00';
+export const followingColor = '#96C200';
+export const notInterestedColor = subTitleColor;
+
+export const selectedBackground = '#E6F2FB';
---
+++ app/component/uikit/touchable.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Touchable from 'react-native-platform-touchable'
+
+const touchable = props => {
+	const effect = props.borderless
+		? Touchable.SelectableBackgroundBorderless()
+		: Touchable.SelectableBackground()
+	return (
+		<Touchable
+			{...props}
+			hitSlop={props.hitSlop}
+			{...(props.foreground ? { foreground: effect } : { background: effect })}
+		>
+			{props.children}
+		</Touchable>
+	)
+}
+
+touchable.defaultProps = {
+	hitSlop: {
+		top: 10,
+		right: 10,
+		bottom: 10,
+		left: 10
+	},
+	borderless: false,
+	foreground: false
+}
+
+touchable.propTypes = {
+	hitSlop: PropTypes.object,
+	borderless: PropTypes.bool,
+	foreground: PropTypes.bool
+}
+
+export default touchable
