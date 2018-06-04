58485d51c114b1e4c4d96d62ac5761ed08e6935a
hbk671104 <hbk671104@gmail.com>
2018-06-04 16:57:01 +0800

    changes

--- app/container/main/portfolio/index.js
+++ app/container/main/portfolio/index.js
@@ -12,63 +12,63 @@ import styles from './style'
 
 @connect()
 export default class Portfolio extends Component {
-	state = {
-		index: 0,
-		routes: [
-			{ key: 'exchangeable', title: '已上所' },
-			{ key: 'unexchangeable', title: '未上所' }
-		]
-	}
+  state = {
+    index: 0,
+    routes: [
+      { key: 'exchangeable', title: '已上所' },
+      { key: 'unexchangeable', title: '未上所' }
+    ]
+  }
 
-	handleIndexChange = index => this.setState({ index })
+  handleIndexChange = index => this.setState({ index })
 
-	renderHeader = props => (
-		<NavBar
-			style={styles.navBar.container}
-			renderTitle={() => (
-				<Text style={styles.navBar.title}>
-					节点
-					<NodeCapIcon name="xiala" />
-				</Text>
-			)}
-			renderBottom={() => (
-				<View>
-					<View style={styles.searchBar.container}>
-						<SearchBarDisplay />
-					</View>
-					<TabBar
-						{...props}
-						style={styles.tabBar.container}
-						labelStyle={styles.tabBar.label}
-						indicatorStyle={styles.tabBar.indicator}
-					/>
-				</View>
-			)}
-		/>
-	)
+  renderHeader = props => (
+    <NavBar
+      style={styles.navBar.container}
+      renderTitle={() => (
+        <Text style={styles.navBar.title}>
+          节点
+          <NodeCapIcon name="xiala" />
+        </Text>
+      )}
+      renderBottom={() => (
+        <View>
+          <View style={styles.searchBar.container}>
+            <SearchBarDisplay />
+          </View>
+          <TabBar
+            {...props}
+            style={styles.tabBar.container}
+            labelStyle={styles.tabBar.label}
+            indicatorStyle={styles.tabBar.indicator}
+          />
+        </View>
+      )}
+    />
+  )
 
-	renderScene = ({ route }) => {
-		switch (route.key) {
-			case 'exchangeable':
-				return <Exchangeable />
-			case 'unexchangeable':
-				return <Unexchangeable />
-			default:
-				return null
-		}
-	}
+  renderScene = ({ route }) => {
+    switch (route.key) {
+      case 'exchangeable':
+        return <Exchangeable />
+      case 'unexchangeable':
+        return <Unexchangeable />
+      default:
+        return null
+    }
+  }
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<TabViewAnimated
-					initialLayout={styles.initialLayout}
-					navigationState={this.state}
-					renderScene={this.renderScene}
-					renderHeader={this.renderHeader}
-					onIndexChange={this.handleIndexChange}
-				/>
-			</View>
-		)
-	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <TabViewAnimated
+          initialLayout={styles.initialLayout}
+          navigationState={this.state}
+          renderScene={this.renderScene}
+          renderHeader={this.renderHeader}
+          onIndexChange={this.handleIndexChange}
+        />
+      </View>
+    )
+  }
 }
