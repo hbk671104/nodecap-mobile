28dd84e9d82691ad480feaabb189aa0a26af658d
hbk671104 <hbk671104@gmail.com>
2018-07-05 12:02:03 +0800

    iphone x adjustment

--- app/component/uikit/safeArea.js
+++ app/component/uikit/safeArea.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import { ifIphoneX } from 'react-native-iphone-x-helper';
 
-const safeArea = props => (
-  <SafeAreaView {...props} forceInset={{ top: 'never' }}>
-    {props.children}
-  </SafeAreaView>
-);
+const safeArea = props => {
+  if (ifIphoneX()) {
+    return (
+      <SafeAreaView {...props} forceInset={{ top: 'never' }}>
+        {props.children}
+      </SafeAreaView>
+    );
+  }
+  return <View {...props}>{props.children}</View>;
+};
 
 export default safeArea;
