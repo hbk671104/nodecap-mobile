257f54a69b8c1a10ab6e7f0990c1696205707d1d
hbk671104 <hbk671104@gmail.com>
2018-07-31 18:47:35 +0800

    minor fix

--- app/utils/style.js
+++ app/utils/style.js
@@ -5,8 +5,8 @@ export const raised = {
   ...Platform.select({
     ios: {
       shadowColor: 'rgba(0,0,0, .4)',
-      shadowOffset: { height: 1, width: 1 },
-      shadowOpacity: 0.5,
+      shadowOffset: { height: 1, width: 0 },
+      shadowOpacity: 0.3,
       shadowRadius: 1,
     },
     android: {
