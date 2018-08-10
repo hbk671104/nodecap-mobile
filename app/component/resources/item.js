20431a9d7007ccff0ad3259653cd6dffbf0c4a64
hbk671104 <hbk671104@gmail.com>
2018-08-10 12:28:25 +0800

    onpress

--- app/component/resources/item.js
+++ app/component/resources/item.js
@@ -4,61 +4,64 @@ import { View, Text } from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import R from 'ramda';
 
+import Touchable from 'component/uikit/touchable';
 import Title from './title';
 import { shadow } from '../../utils/style';
 
-const resources = ({ data }) => {
+const resources = ({ data, onPress }) => {
   const types = R.pathOr([], ['types'])(data);
   return (
-    <View style={styles.container}>
-      <View style={styles.top.container}>
-        <Text style={styles.top.title}>{data.name}</Text>
-        {!R.isEmpty(types) && (
-          <Text style={styles.top.divider}>
-            {'  '}|{'  '}
-            {types.map(t => (
-              <Title key={t.id} data={t} />
-            ))}
-          </Text>
-        )}
-      </View>
-      <Grid style={styles.bottom.container}>
-        <Row>
-          <Col>
-            <Text style={styles.bottom.group.title}>
-              机构：
-              <Text style={styles.bottom.group.content}>{data.org}</Text>
+    <Touchable onPress={onPress}>
+      <View style={styles.container}>
+        <View style={styles.top.container}>
+          <Text style={styles.top.title}>{data.name}</Text>
+          {!R.isEmpty(types) && (
+            <Text style={styles.top.divider}>
+              {'  '}|{'  '}
+              {types.map(t => (
+                <Title key={t.id} data={t} />
+              ))}
             </Text>
-          </Col>
-          <Col>
-            {!!data.title && (
+          )}
+        </View>
+        <Grid style={styles.bottom.container}>
+          <Row>
+            <Col>
               <Text style={styles.bottom.group.title}>
-                职位：
-                <Text style={styles.bottom.group.content}>{data.title}</Text>
+                机构：
+                <Text style={styles.bottom.group.content}>{data.org}</Text>
               </Text>
-            )}
-          </Col>
-        </Row>
-        <Row style={{ marginTop: 8 }}>
-          <Col>
-            {!!data.mobile && (
-              <Text style={styles.bottom.group.title}>
-                手机：
-                <Text style={styles.bottom.group.content}>{data.mobile}</Text>
-              </Text>
-            )}
-          </Col>
-          <Col>
-            {!!data.wechat && (
-              <Text style={styles.bottom.group.title}>
-                微信：
-                <Text style={styles.bottom.group.content}>{data.wechat}</Text>
-              </Text>
-            )}
-          </Col>
-        </Row>
-      </Grid>
-    </View>
+            </Col>
+            <Col>
+              {!!data.title && (
+                <Text style={styles.bottom.group.title}>
+                  职位：
+                  <Text style={styles.bottom.group.content}>{data.title}</Text>
+                </Text>
+              )}
+            </Col>
+          </Row>
+          <Row style={{ marginTop: 8 }}>
+            <Col>
+              {!!data.mobile && (
+                <Text style={styles.bottom.group.title}>
+                  手机：
+                  <Text style={styles.bottom.group.content}>{data.mobile}</Text>
+                </Text>
+              )}
+            </Col>
+            <Col>
+              {!!data.wechat && (
+                <Text style={styles.bottom.group.title}>
+                  微信：
+                  <Text style={styles.bottom.group.content}>{data.wechat}</Text>
+                </Text>
+              )}
+            </Col>
+          </Row>
+        </Grid>
+      </View>
+    </Touchable>
   );
 };
 
@@ -105,6 +108,11 @@ const styles = {
 
 resources.propTypes = {
   data: PropTypes.object.isRequired,
+  onPress: PropTypes.func,
+};
+
+resources.defaultProps = {
+  onPress: () => null,
 };
 
 export default resources;
