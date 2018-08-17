a581f126afe7609185a0d29e96421d9f2e590028
hbk671104 <hbk671104@gmail.com>
2018-08-17 16:26:48 +0800

    done with bar

--- app/container/main/fund/project/index.js
+++ app/container/main/fund/project/index.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import ScrollableTabView, {
+  ScrollableTabBar,
+} from 'react-native-scrollable-tab-view';
 
 import NavBar from 'component/navBar';
+import Touchable from 'component/uikit/touchable';
+import ProjectList from './list';
 import styles from './style';
 
 class FundProject extends Component {
+  renderHeader = () => (
+    <ScrollableTabBar
+      style={styles.tab.container}
+      underlineStyle={styles.tab.underline}
+      renderTab={(name, page, isTabActive, onPressHandler, onLayoutHandler) => (
+        <Touchable
+          key={`${name}_${page}`}
+          style={styles.tab.item.container}
+          onPress={() => onPressHandler(page)}
+          onLayout={onLayoutHandler}
+        >
+          <Text
+            style={[
+              styles.tab.item.title,
+              isTabActive && styles.tab.item.highlight,
+            ]}
+          >
+            {name}
+          </Text>
+        </Touchable>
+      )}
+    />
+  );
+
   render() {
     return (
       <View style={styles.container}>
         <NavBar back gradient title="项目清单" />
+        <ScrollableTabView
+          prerenderingSiblingsNumber={Infinity}
+          renderTabBar={this.renderHeader}
+        >
+          <ProjectList tabLabel="全部" />
+          <ProjectList tabLabel="已上所" />
+          <ProjectList tabLabel="未上所" />
+          <ProjectList tabLabel="未出货" />
+          <ProjectList tabLabel="部分出货" />
+          <ProjectList tabLabel="完全出货" />
+        </ScrollableTabView>
       </View>
     );
   }
---
+++ app/container/main/fund/project/item.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text } from 'react-native';
+
+import Touchable from 'component/uikit/touchable';
+import Avatar from 'component/uikit/avatar';
+
+const item = ({ data, onPress }) => (
+  <Touchable onPress={onPress}>
+    <View style={styles.container}>
+      <Avatar size={37} />
+      <View style={styles.content.container}>
+        <Text style={styles.content.title}>
+          Aelf
+          {'  '}
+          <Text style={styles.content.subtitle}>(ELF)</Text>
+        </Text>
+      </View>
+    </View>
+  </Touchable>
+);
+
+const styles = {
+  container: {
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content: {
+    container: {
+      marginLeft: 8,
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: 16,
+      color: 'rgba(0, 0, 0, 0.85)',
+    },
+    subtitle: {
+      fontWeight: 'bold',
+      fontSize: 13,
+      color: 'rgba(0, 0, 0, 0.65)',
+    },
+  },
+};
+
+item.propTypes = {
+  data: PropTypes.object,
+  onPress: PropTypes.func,
+};
+
+export default item;
