dc2d7203acce4bcc69a1667d6684df734850a084
hbk671104 <hbk671104@gmail.com>
2018-09-25 18:14:17 +0800

    add trending item regex

--- app/component/news/trending.js
+++ app/component/news/trending.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import R from 'ramda';
 
 import styles from './style';
 
-const trending = props => {
+const trending = ({ data }) => {
+  const regex = R.pipe(
+    R.path(['content']),
+    R.match(/【(.*)】(.*)/),
+  )(data);
+
+  const title = R.pathOr('', [1])(regex);
+  const content = R.pathOr('', [2])(regex);
+
   return (
     <View>
       <View>
