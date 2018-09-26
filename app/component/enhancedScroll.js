a1cacf01e0ab060ffb134e9eceb9eefe7da36972
hbk671104 <hbk671104@gmail.com>
2018-09-26 08:33:48 +0800

    optimized scrollview tap event

--- app/component/enhancedScroll.js
+++ app/component/enhancedScroll.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const enhancedScroll = props => (
-  <KeyboardAwareScrollView {...props} enableOnAndroid>
+  <KeyboardAwareScrollView
+    {...props}
+    enableOnAndroid
+    keyboardShouldPersistTaps="handled"
+  >
     {props.children}
   </KeyboardAwareScrollView>
 );
