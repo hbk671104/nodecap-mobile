7209dae30600f998314335df5ea268a22c9c5427
刘剑锋 <i@moejser.com>
2018-10-10 17:35:37 +0800

    「上所公告详情」上所公告详情页面字体为宋体，与整个系统字体不统一，用户跳出感比较强

--- app/services/htmlWraper.js
+++ app/services/htmlWraper.js
@@ -5,6 +5,24 @@ export default function htmlWrapper(content) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">  
+        <style>
+                html, body, div, span, applet, object, iframe,
+        h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+        a, abbr, acronym, address, big, cite, code,
+        del, dfn, em, img, ins, kbd, q, s, samp,
+        small, strike, strong, sub, sup, tt, var,
+        b, u, i, center,
+        dl, dt, dd, ol, ul, li,
+        fieldset, form, label, legend,
+        table, caption, tbody, tfoot, thead, tr, th, td,
+        article, aside, canvas, details, embed, 
+        figure, figcaption, footer, header, hgroup, 
+        menu, nav, output, ruby, section, summary,
+        time, mark, audio, video {
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+          border: none !important;
+        }
+</style>
     </head>
     <body>
         ${content}
