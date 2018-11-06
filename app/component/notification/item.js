a077f371c880a1ec7dc46cbfbd2182ab819258b9
hbk671104 <hbk671104@gmail.com>
2018-11-06 18:06:06 +0800

    notification item styling

--- app/component/notification/item.js
+++ app/component/notification/item.js
@@ -17,13 +17,25 @@ const notificationItem = ({ data, onPress, onPressShare }) => {
   const subtitle = R.pathOr('--', ['subtitle'])(data);
   const push_at = R.pathOr(null, ['push_at'])(data);
   const source = R.pathOr('', ['source'])(data);
+  const logo_url = R.pathOr('', ['logo_url'])(data);
 
   return (
     <Touchable foreground onPress={onPress(data.id)}>
       <View style={styles.container}>
         <Flex justify="between" style={styles.header}>
-          <Text style={styles.sourceName}>{source}</Text>
-          <Text style={styles.pushAt}>{push_at ? moment(push_at).format('HH:mm') : null}</Text>
+          <Flex align="center">
+            <Avatar
+              raised={false}
+              innerRatio={1}
+              size={20}
+              style={styles.avatar}
+              source={{ uri: logo_url }}
+            />
+            <Text style={styles.sourceName}>{source}</Text>
+          </Flex>
+          <Text style={styles.pushAt}>
+            {push_at ? moment.unix(push_at).format('HH:mm') : null}
+          </Text>
         </Flex>
         <View style={styles.content}>
           <Text style={styles.title}>{title}</Text>
@@ -37,7 +49,10 @@ const notificationItem = ({ data, onPress, onPressShare }) => {
           </View>
           <Touchable foreground onPress={() => onPressShare(data)}>
             <Flex align="center">
-              <Image style={{ width: 11, height: 11 }}source={require('asset/project/detail/share.png')} />
+              <Image
+                style={{ width: 11, height: 11 }}
+                source={require('asset/project/detail/share.png')}
+              />
               <Text style={styles.share}>分享</Text>
             </Flex>
           </Touchable>
@@ -51,6 +66,11 @@ const styles = {
   container: {
     backgroundColor: 'white',
   },
+  avatar: {
+    height: 20,
+    width: 20,
+    marginRight: 10,
+  },
   header: {
     flex: 1,
     height: 35,
@@ -58,14 +78,36 @@ const styles = {
     borderBottomColor: '#e9e9e9',
     borderBottomWidth: 0.5,
   },
-  sourceName: { fontSize: 13, color: 'rgba(0,0,0,0.85)', letterSpacing: 0.16, fontFamily: 'PingFangSC-Medium' },
-  pushAt: { fontSize: 13, color: 'rgba(0,0,0,0.45)', letterSpacing: 0.18, lineHeight: 19 },
+  sourceName: {
+    fontSize: 13,
+    color: 'rgba(0,0,0,0.85)',
+    letterSpacing: 0.16,
+    fontFamily: 'PingFangSC-Medium',
+  },
+  pushAt: {
+    fontSize: 13,
+    color: 'rgba(0,0,0,0.45)',
+    letterSpacing: 0.18,
+    lineHeight: 19,
+  },
   content: {
     paddingHorizontal: 12,
     paddingVertical: 15,
   },
-  title: { fontFamily: 'PingFangSC-Medium', fontSize: 15, color: 'rgba(0,0,0,0.85)', letterSpacing: 0.18, lineHeight: 22 },
-  subtitle: { fontSize: 13, color: 'rgba(0,0,0,0.65)', letterSpacing: 0.16, lineHeight: 19, marginTop: 5 },
+  title: {
+    fontFamily: 'PingFangSC-Medium',
+    fontSize: 15,
+    color: 'rgba(0,0,0,0.85)',
+    letterSpacing: 0.18,
+    lineHeight: 22,
+  },
+  subtitle: {
+    fontSize: 13,
+    color: 'rgba(0,0,0,0.65)',
+    letterSpacing: 0.16,
+    lineHeight: 19,
+    marginTop: 5,
+  },
   footer: {
     paddingHorizontal: 12,
     paddingVertical: 10,
@@ -76,8 +118,19 @@ const styles = {
     backgroundColor: '#eaeaea',
     borderRadius: 1,
   },
-  coinNameText: { fontSize: 11, color: 'rgba(0,0,0,0.45)', letterSpacing: 0.13, textAlign: 'justify' },
-  share: { fontSize: 12, color: 'rgba(0,0,0,0.45)', letterSpacing: 0.17, lineHeight: 19, marginLeft: 5 },
+  coinNameText: {
+    fontSize: 11,
+    color: 'rgba(0,0,0,0.45)',
+    letterSpacing: 0.13,
+    textAlign: 'justify',
+  },
+  share: {
+    fontSize: 12,
+    color: 'rgba(0,0,0,0.45)',
+    letterSpacing: 0.17,
+    lineHeight: 19,
+    marginLeft: 5,
+  },
 };
 
 notificationItem.propTypes = {
