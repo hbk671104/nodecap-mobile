a1bc0a949e677b12bd13e8e4c684d966f43785bd
hbk671104 <hbk671104@gmail.com>
2018-11-07 11:50:42 +0800

    error handling capable avatar

--- app/component/uikit/avatar.js
+++ app/component/uikit/avatar.js
@@ -1,47 +1,67 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { View, Image, Animated } from 'react-native';
+import { Animated } from 'react-native';
 import R from 'ramda';
-import { raised } from '../../utils/style';
+import { raised as raisedStyle } from '../../utils/style';
 import SolidAvatar from './avatar.solid';
 
-const avatar = props => {
-  const uri = R.pathOr('', ['source', 'uri'])(props);
-  return (
-    <Animated.View
-      style={[
-        styles.container,
-        { height: props.size, width: props.size, borderRadius: props.size / 2 },
-        props.raised && { ...raised },
-        props.style,
-      ]}
-    >
-      <Animated.Image
-        {...props}
-        resizeMode={props.resizeMode}
-        source={
-          R.isEmpty(uri)
-            ? require('asset/project/project_logo_default.png')
-            : { uri }
-        }
-        style={{
-          height: props.size * props.innerRatio,
-          width: props.size * props.innerRatio,
-          borderRadius: (props.size * props.innerRatio) / 2,
-        }}
-      />
-    </Animated.View>
-  );
-};
+class Avatar extends PureComponent {
+  constructor(props) {
+    super(props);
+    const uri = R.pathOr('', ['source', 'uri'])(props);
+    this.state = {
+      source: R.isEmpty(uri)
+        ? require('asset/project/project_logo_default.png')
+        : { uri },
+    };
+  }
 
-avatar.defaultProps = {
+  onError = error => {
+    this.setState({
+      source: require('asset/project/project_logo_default.png'),
+    });
+  };
+
+  render() {
+    const { source } = this.state;
+    const { style, size, raised, resizeMode, innerRatio } = this.props;
+    return (
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            height: size,
+            width: size,
+            borderRadius: size / 2,
+          },
+          raised && { ...raisedStyle },
+          style,
+        ]}
+      >
+        <Animated.Image
+          {...this.props}
+          resizeMode={resizeMode}
+          source={source}
+          style={{
+            height: size * innerRatio,
+            width: size * innerRatio,
+            borderRadius: (size * innerRatio) / 2,
+          }}
+          onError={this.onError}
+        />
+      </Animated.View>
+    );
+  }
+}
+
+Avatar.defaultProps = {
   size: 42,
   innerRatio: 2 / 3,
   resizeMode: 'contain',
   raised: true,
 };
 
-avatar.propTypes = {
+Avatar.propTypes = {
   size: PropTypes.number,
   innerRatio: PropTypes.number,
   resizeMode: PropTypes.string,
@@ -57,4 +77,4 @@ const styles = {
 };
 
 export { SolidAvatar };
-export default avatar;
+export default Avatar;
