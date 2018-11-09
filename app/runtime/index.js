6f071980182a7deb18e4a8beb853a408e8ff5d3b
刘剑锋 <i@moejser.com>
2018-11-09 16:51:09 +0800

    runtime config

--- app/runtime/index.js
+++ app/runtime/index.js
@@ -3,11 +3,10 @@ import development from './development';
 import staging from './staging';
 import production from './production';
 
-const runtimeConfig =
-  Config.API_URL === 'http://api-staging.hotnode.cn/v1'
-    ? global.__DEV__
-      ? development
-      : staging
-    : production;
+const envMap = {
+  development,
+  staging,
+  production,
+};
 
-export default runtimeConfig;
+export default Config.ENV ? envMap[Config.ENV] : production;
