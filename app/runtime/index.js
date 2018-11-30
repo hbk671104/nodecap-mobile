ef7a2eb95c73644d174b06004e713f9f206a6c30
刘剑锋 <i@moejser.com>
2018-12-01 00:11:42 +0800

    runtime

--- app/runtime/index.js
+++ app/runtime/index.js
@@ -9,5 +9,4 @@ const envMap = {
   production,
 };
 
-export default staging;
-// export default (Config.ENV ? envMap[Config.ENV] : production);
+export default (Config.ENV ? envMap[Config.ENV] : production);
