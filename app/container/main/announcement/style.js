a59c45526dc5fd13f4df2d50582dd3f3c7046bac
hbk671104 <hbk671104@gmail.com>
2018-12-20 11:01:10 +0800

    bottom floating button in single service wrapper

--- app/container/main/announcement/style.js
+++ app/container/main/announcement/style.js
@@ -1,7 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import { shadow } from 'utils/style';
-
-const deviceWidth = Dimensions.get('window').width;
+import { StyleSheet } from 'react-native';
 
 export default {
   container: {
@@ -78,35 +75,6 @@ export default {
           fontWeight: 'bold',
         },
       },
-    },
-  },
-  button: {
-    wrapper: {
-      position: 'absolute',
-      bottom: 24,
-      left: (deviceWidth - 160) / 2,
-      ...shadow,
-      shadowColor: '#1890FF',
-      shadowOffset: {
-        width: StyleSheet.hairlineWidth,
-        height: 4,
-      },
-      shadowRadius: 4,
-      shadowOpacity: 0.5,
-      backgroundColor: '#1890FF',
-      borderRadius: 22,
-    },
-    container: {
-      height: 44,
-      width: 160,
-      borderRadius: 22,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 13,
-      fontWeight: 'bold',
-      color: 'white',
     },
   },
 };
